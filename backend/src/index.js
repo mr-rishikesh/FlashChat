93508ea55d50f4ca82271b8ed47749da7fd2b742
mr-rishikesh <mr.rishikesh2@gmail.com>
2025-02-24 22:55:53 +0530

    Application completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,13 @@ import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors" 
 import {app , server} from "./lib/socket.js"
+import path from "path";
 
 dotenv.config();
 
 
 
-
+const __dirname = path.resolve();
 connectDB()
 app.use(express.json({ limit: '10mb' })); 
 app.use(cookieParser());
@@ -22,6 +23,15 @@ app.use(cors({
 
 app.use("/api/auth" , router)
 app.use("/api/messages" , messageRouter)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
 const PORT = process.env.PORT
 server.listen( PORT, () => {
     console.log(`App is listing at port ${PORT}`)
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios.js"
 import { io } from "socket.io-client"
-const BASE_URL = "http://localhost:5001"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/a";
 import toast from "react-hot-toast"
 
 
